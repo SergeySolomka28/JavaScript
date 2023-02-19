bcae0079461ce7acbe8c51b80afe0ba8ee8c4f1f
SeregaSolomka28 <1994solomka@gmail.com>
2023-02-20 01:12:32 +0200

    Home Work

---
+++ HomeWork/Les6/script6.js
@@ -0,0 +1,91 @@
+// - Знайти та вивести довижину настипних стрінгових значень
+// 'hello world', 'lorem ipsum', 'javascript is cool'
+
+let lenth = (string) => {
+    string.length;
+    console.log(string.length)
+
+}
+lenth('hello world')
+lenth('lorem ipsum')
+lenth('javascript is cool')
+
+// - Перевести до великого регістру наступні стрінгові значення
+
+let str = (string1) => {
+    console.log(string1.toUpperCase())
+}
+
+str('hello world')
+str('lorem ipsum')
+str('javascript is cool')
+
+// 'hello world', 'lorem ipsum', 'javascript is cool'
+
+// - Перевести до нижнього регістру настyпні стрінгові значення
+console.log('HELLO WORLD'.toLowerCase());
+console.log('LOREM IPSUM'.toLowerCase());
+console.log('JAVASCRIPT IS COOL'.toLowerCase());
+// Є "брудна" стрінга
+//
+let str1 = ' dirty string   ';
+console.log(str1.trim());
+// . Почистити її від зайвих пробілів.
+//
+// - Напишіть функцію stringToarray(str), яка перетворює рядок на масив слів.
+let str2 = 'Ревуть воли як ясла повні';
+let stringToArray = (string) => {
+    let arrString = string.split(' ');
+    return arrString;
+
+}
+console.log(stringToArray(str2));
+
+// let arr = stringToarray(str); ['Ревуть', 'воли', 'як', 'ясла', 'повні']
+//
+// - є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map  перетворити всі об'єкти в масиві на стрінгові.
+// - створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки в залежності від значення аргументу direction.
+//     let nums = [11,21,3];
+// sortNums(nums,'ascending') // [3,11,21]
+// sortNums(nums,'descending') // [21,11,3]
+//
+// ==========================
+// - є масив
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+// -- відсортувати його за спаданням за monthDuration
+// -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+// -- за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration}
+// =========================
+//     описати колоду карт (від 6 до туза без джокерів)
+// - знайти піковий туз
+// - всі шістки
+// - всі червоні карти
+// - всі буби
+// - всі трефи від 9 та більше
+//
+// {
+//     cardSuit: '', // 'spade', 'diamond','heart', 'clubs'
+//         value: '', // '6'-'10', 'ace','jack','queen','king','joker'
+//     color:'', // 'red','black'
+// }
+//
+// =========================
+//
+//     Взяти описану колоду карт, та за допомоги reduce упакувати всі карти по "мастях" в об'єкт
+// {
+//     spades:[],
+//         diamonds:[],
+//     hearts:[],
+//     clubs:[]
+// }
+// =========================
+//     взяти з arrays.js (який лежить в папці 2023 plan) масив coursesArray
+// --написати пошук всіх об'єктів, в який в modules є sass
+// --написати пошук всіх об'єктів, в який в modules є docker
