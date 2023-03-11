157a644446559b250e606d96c3665b53f52b8458
SeregaSolomka28 <1994solomka@gmail.com>
2023-03-12 00:00:37 +0200

    Home Work 8

--- HomeWork/Les8/script8.js
+++ HomeWork/Les8/script8.js
@@ -1,62 +1,62 @@
 // - Створити функцію конструктор для об'єктів User з полями id, name, surname , email, phone
 
-// function User(id, name, surname, email, phone) {
-//     this.id = id;
-//     this.name = name;
-//     this.surname = surname;
-//     this.email = email;
-//     this.phone = phone;
-// }
+function User(id, name, surname, email, phone) {
+    this.id = id;
+    this.name = name;
+    this.surname = surname;
+    this.email = email;
+    this.phone = phone;
+}
 
-// let user = new User(11, 'kolya', 'pupkin', 'pupkin@gmail.com', 0993572985);
-// console.log(user);
-// // створити пустий масив, наповнити його 10 об'єктами new User(....)
-// let arrUser = [
-//     new User(31, 'kolya', 'pupkin', 'pupkin@gmail.com', 0992372985),
-//     new User(11, 'vasya', 'surkin', 'surkin@gmail.com', 0992372985),
-//     new User(16, 'dima', 'gulkin', 'gulkin@gmail.com', 0992374485),
-//     new User(12,'ivan','ivanov','ivanov@gmail.com',09899334857),
-//     new User(48,'gregory','bilyak','bilyak@gmail.com', 0634563345),
-//     new User(99, 'mike','adamov','adamov@gmail.com',0678983452),
-//     new User(13,'galina' ,'petrova','petrova@gmail.com',0634567898),
-//     new User(77,'tanya','sinisa','sinisa@gmail.com',0635462134),
-//     new User(76,'petro','pyatochkin','pyatochkin@gmail.com',0998329988),
-//     new User(33,'rafael','redukevich','redukevich@gmail.com',0934488992)
-// ]
-//
-// console.log(arrUser);
-// // - Взяти масив з  User[] з попереднього завдання, та відфільтрувати , залишивши тільки об'єкти з парними id (filter)
-// let filter=arrUser.filter((item=>item.id%2===0));
-// console.log(filter);
-//
-// // - Взяти масив з  User[] з попереднього завдання, та відсортувати його по id. по зростанню (sort)
-// let sort=arrUser.sort((a,b)=>a.id-b.id)
-// console.log(sort);
+let user = new User(11, 'kolya', 'pupkin', 'pupkin@gmail.com', 0993572985);
+console.log(user);
+// створити пустий масив, наповнити його 10 об'єктами new User(....)
+let arrUser = [
+    new User(31, 'kolya', 'pupkin', 'pupkin@gmail.com', 0992372985),
+    new User(11, 'vasya', 'surkin', 'surkin@gmail.com', 0992372985),
+    new User(16, 'dima', 'gulkin', 'gulkin@gmail.com', 0992374485),
+    new User(12,'ivan','ivanov','ivanov@gmail.com',09899334857),
+    new User(48,'gregory','bilyak','bilyak@gmail.com', 0634563345),
+    new User(99, 'mike','adamov','adamov@gmail.com',0678983452),
+    new User(13,'galina' ,'petrova','petrova@gmail.com',0634567898),
+    new User(77,'tanya','sinisa','sinisa@gmail.com',0635462134),
+    new User(76,'petro','pyatochkin','pyatochkin@gmail.com',0998329988),
+    new User(33,'rafael','redukevich','redukevich@gmail.com',0934488992)
+]
+
+console.log(arrUser);
+// - Взяти масив з  User[] з попереднього завдання, та відфільтрувати , залишивши тільки об'єкти з парними id (filter)
+let filter=arrUser.filter((item=>item.id%2===0));
+console.log(filter);
+
+// - Взяти масив з  User[] з попереднього завдання, та відсортувати його по id. по зростанню (sort)
+let sort=arrUser.sort((a,b)=>a.id-b.id)
+console.log(sort);
 
 // - створити класс для об'єктів Client з полями id, name, surname , email, phone, order (поле є масивом зі списком товарів)
 // створити пустий масив, наповнити його 10 об'єктами Client
 // - Взяти масив (Client [] з попереднього завдання).Відсортувати його по кількості товарів в полі order по зростанню. (sort)
-// class Clients {
-//     constructor(id, name, surname, email, phone, order) {
-//         User.call(this, id, name, surname, email, phone)
-//         this.order = order;
-//     }
-// }
+class Clients {
+    constructor(id, name, surname, email, phone, order) {
+        User.call(this, id, name, surname, email, phone)
+        this.order = order;
+    }
+}
 
-// let arrClients=[
-//     new Clients(31, 'kolya', 'pupkin', 'pupkin@gmail.com', 0992372985, ['laptop','mouse','phone']),
-//     new Clients(11, 'vasya', 'surkin', 'surkin@gmail.com', 0992372985, ['laptop','mouse','phone','book']),
-//     new Clients(16, 'dima', 'gulkin', 'gulkin@gmail.com', 0992374485, ['car','pensil']),
-//     new Clients(12,'ivan','ivanov','ivanov@gmail.com',09899334857 , ['car','pensil','table']),
-//     new Clients(48,'gregory','bilyak','bilyak@gmail.com', 0634563345,['car','pensil',]),
-//     new Clients(92, 'mike','adamov','adamov@gmail.com',067898345677, ['car','pensil']),
-//     new Clients(13,'galina' ,'petrova','petrova@gmail.com',0634567898, ['apple']),
-//     new Clients(77,'tanya','sinisa','sinisa@gmail.com',0635462134,['water']),
-//     new Clients(76,'petro','pyatochkin','pyatochkin@gmail.com',0998329988, ['table','apple']),
-//     new Clients(33,'rafael','redukevich','redukevich@gmail.com',0934488992, ['guitar'])
-// ]
-// let sortClient = arrClients.sort((a,b)=>a.order.length-b.order.length );
-// console.log(sortClient);
+let arrClients=[
+    new Clients(31, 'kolya', 'pupkin', 'pupkin@gmail.com', 0992372985, ['laptop','mouse','phone']),
+    new Clients(11, 'vasya', 'surkin', 'surkin@gmail.com', 0992372985, ['laptop','mouse','phone','book']),
+    new Clients(16, 'dima', 'gulkin', 'gulkin@gmail.com', 0992374485, ['car','pensil']),
+    new Clients(12,'ivan','ivanov','ivanov@gmail.com',09899334857 , ['car','pensil','table']),
+    new Clients(48,'gregory','bilyak','bilyak@gmail.com', 0634563345,['car','pensil',]),
+    new Clients(92, 'mike','adamov','adamov@gmail.com',067898345677, ['car','pensil']),
+    new Clients(13,'galina' ,'petrova','petrova@gmail.com',0634567898, ['apple']),
+    new Clients(77,'tanya','sinisa','sinisa@gmail.com',0635462134,['water']),
+    new Clients(76,'petro','pyatochkin','pyatochkin@gmail.com',0998329988, ['table','apple']),
+    new Clients(33,'rafael','redukevich','redukevich@gmail.com',0934488992, ['guitar'])
+]
+let sortClient = arrClients.sort((a,b)=>a.order.length-b.order.length );
+console.log(sortClient);
 // - Створити функцію конструктор яка дозволяє створювати об'єкти car, з властивостями модель, виробник, рік випуску, максимальна швидкість, об'єм двигуна. додати в об'єкт функції:
 // -- drive () - яка виводить в консоль `їдемо зі швидкістю ${максимальна швидкість} на годину`
 // -- info () - яка виводить всю інформацію про автомобіль в форматі `назва поля - значення поля`
@@ -201,25 +201,39 @@ cars1.drive()
 // Сторити об'єкт класу "принц" за допомоги класу який має поля ім'я, вік, туфелька яку він знайшов.
 //     За допомоги циклу знайти яка попелюшка повинна бути з принцом.
 //     Додатково, знайти необхідну попелюшку за допомоги функції масиву find та відповідного колбеку
-
-
-class Princesa {
-    constructor(name, age, legSize) {
+class Human {
+    constructor(name, age) {
         this.name = name;
         this.age = age;
+    }
+}
+
+class Princesa extends Human {
+    constructor(name, age, legSize) {
+        super(name, age);
         this.legSize = legSize;
     }
 }
 
-class Princ {
+class Princ extends Human {
     constructor(name, age, findSize) {
-        this.name = name;
-        this.age = age;
+        super(name, age);
         this.findSize = findSize;
     }
-    find(arr){
-        return arr.find((Princesa)=>princ.findSize===this.legSize)
+
+    find1(arr) {
+        for (const item of arr) {
+            if (princ.findSize === item.legSize) {
+                // return item;
+                console.log(item);
+            }
+        }
     }
+
+    find2(arr) {
+         return arr.find((item) => item.legSize === this.findSize);
+     }
+
 }
 
 let princ = new Princ('Sergey', 30, 38);
@@ -233,9 +247,6 @@ const princesaArr = [
     new Princesa('Inna', 18, 35),
     new Princesa('Tanya', 38, 36),
 ];
-for (const princesa of princesaArr) {
-    if (princ.findSize === princesa.legSize) {
-        console.log(princesa);
-    }
-}
-console.log(princesaArr.find())
+
+(princ.find1(princesaArr))
+console.log(princ.find2(princesaArr))
